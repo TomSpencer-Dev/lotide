b982d283839701e3c2aa35e05f2c7829487c01c7
Tom Spencer <thomas.spencer2034@gmail.com>
2023-08-28 13:44:14 -0400

    Added leeterPositions.js file with letterPositions function that will return indexes of letters

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++)
+  /*for (let letter of sentence)*/ {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+        results[sentence[i]].push(i);
+      } else {
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+    
+  } return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1,arr2)) {
+    console.log(`😃 Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`😒 Assertion Failed: ${arr1} !==  ${arr2}`);
+  }
+};
+
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2,3]);
+assertArraysEqual(letterPositions("hello").o, [4]);
