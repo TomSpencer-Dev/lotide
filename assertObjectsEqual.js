7e34ff67ab6607ec7d5cb3cbf680dac2f9e1f63f
Tom Spencer <thomas.spencer2034@gmail.com>
2023-08-29 13:23:30 -0400

    Added comments to eqObjects function

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -25,24 +25,29 @@ const eqObjects = function(object1, object2) {
   const obj1Arr = Object.keys(object1);
   const obj2Arr = Object.keys(object2);
 
-
+  //Checking if both objects have an equal number of keys and comparing values
   if (obj1Arr.length === obj2Arr.length) {
     for (let key of obj1Arr) {
+      //Check if key value is an array to check if arrays are equal
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-        return eqArrays(object1[key], object2[key]);
-      }
-      
-      if (object1[key] !== object2[key]) {
-        return false;
+        if (!eqArrays(object1[key], object2[key])) {
+          return false;
+        }
+        //Else compare primitive values
       } else {
-        return true;
+        if (object1[key] !== object2[key]) {
+          return false;
+        }
+
       }
-    }
+  
+    } return true;
   } else {
     return false;
   }
       
 };
+
 
 const shirtObject = { color: "red", size: "medium" };
 const anotherShirtObject = { size: "medium", color: "red" };
--- eqObjects.js
+++ eqObjects.js
@@ -20,18 +20,19 @@ const eqArrays = function(arr1, arr2) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back false
-
 const eqObjects = function(object1, object2) {
   const obj1Arr = Object.keys(object1);
   const obj2Arr = Object.keys(object2);
 
-
+  //Checking if both objects have an equal number of keys and comparing values
   if (obj1Arr.length === obj2Arr.length) {
     for (let key of obj1Arr) {
+      //Check if key value is an array to check if arrays are equal
       if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
         if (!eqArrays(object1[key], object2[key])) {
           return false;
         }
+        //Else compare primitive values
       } else {
         if (object1[key] !== object2[key]) {
           return false;
